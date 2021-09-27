3c7d495abe49825bdc0bd1b18392857f111113c5
Erick Ferrareto <e.ferraretowork@gmail.com>
2021-09-27 11:49:58 -0300

    commit

--- lets.js
+++ lets.js
@@ -1,5 +1,5 @@
 function retornar(){
-    let nome = document.getElementById('nome')
+    let nome = document.getElementById('pme')
     let n = (nome.value)
     alert(`Olá ${n}, é bom ter você novamente.`)
 }
